febfa07216c04b4a0465b20de88c090cd1343658
Plopmenz <plopmenz@gmail.com>
2025-04-03 12:18:22 +0200

    install download link fix

--- os/install.sh
+++ os/install.sh
@@ -5,7 +5,7 @@ set -e # Stop on error
 # Install kexec on Ubuntu
 apt install kexec-tools
 
-curl -L https://github.com/Openmesh-Network/xnode-manager/releases/OSkexec/download/OSkexec.tar.gz | tar -xzf- -C /root
+curl -L https://github.com/Openmesh-Network/xnode-manager/releases/download/OSkexec/OSkexec.tar.gz | tar -xzf- -C /root
 
 # Boot into kexec
 /root/kexec-boot
