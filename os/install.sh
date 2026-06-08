47acb7109aa1b5cd77b94c446444de54ed1cc7d6
Plopmenz <plopmenz@gmail.com>
2026-06-08 07:47:15 +0000

    migrate to xnodeos

--- os/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e # Stop on error
-
-# Download and extract kexec archive
-curl -L "https://github.com/Openmesh-Network/xnode-manager/releases/download/installer/xnodeos-kexec-installer-$(uname -m)-linux.tar.gz" | tar -xzf- -C /root
-
-# Boot into kexec
-/root/xnodeos/install
